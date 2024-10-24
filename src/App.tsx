827ea7787c7c9cae73d6fccd858157443603ae4c
atezer <git@stackblitz.com>
2024-10-24 10:40:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import UXMaturityChecklist from './uicomponent';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <UXMaturityChecklist />
+    </>
+  );
+}
+
+export default App;
